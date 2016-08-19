50e95cbabf5cab462ef4a74f17489ffadd852430
abinocoe <abigailcoe@gmail.com>
2016-08-19 23:17:47 +0100

    added ability to toggle class of clicked elements. went too far and should have commited earlier. very confused rn.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
 var litUp = litUp || {};
 
 litUp.setup = function() {
-  $('li').click(litUp.alight)
-  
-litUp.alight = function() {
-  console.log(event.target.value);
+  $('li').click(litUp.getIndex);
+}
+
+litUp.getIndex = function() {
+  var index = $('ul').index(this);
+  litUp.alight();
+}
+
+litUp.alight = function(index) {
+  console.log(index)
+  $(event.target).toggleClass("light");
+
 }
 
 $(function() { litUp.setup() });
