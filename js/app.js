1dc8e0fa932d6733f81934a2d7bc981ef7478cbf
abinocoe <abigailcoe@gmail.com>
2016-08-19 19:15:41 +0100

    started js. declared game object and implemented jQuery event listeners

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
-console.log("hello")
+var litUp = litUp || {};
 
+litUp.setup = function() {
+  $('li').click(litUp.alight)
+  
+litUp.alight = function() {
+  console.log(event.target.value);
+}
+
+$(function() { litUp.setup() });
