1fbd088bc707096ee46115f7b03d6a7ada57bb15
abinocoe <abigailcoe@gmail.com>
2016-08-20 13:19:12 +0100

    added function to check for win and updated initial html to display partly lit board

--- js/app.js
+++ js/app.js
@@ -10,56 +10,58 @@ litUp.getIndex = function() {
   var a       = Math.sqrt($boxes.length);
   if (index % a === 0) {
     if (index > 0 && index < ($boxes.length)-a) {
-      $($boxes[index]).toggleClass('light')
-      $($boxes[index+1]).toggleClass('light')
-      $($boxes[index-a]).toggleClass('light')
-      $($boxes[index+a]).toggleClass('light')
+      $($boxes[index]).toggleClass('light');
+      $($boxes[index+1]).toggleClass('light');
+      $($boxes[index-a]).toggleClass('light');
+      $($boxes[index+a]).toggleClass('light');
     } else if (index === 0) {
-      $($boxes[index]).toggleClass('light')
-      $($boxes[index+1]).toggleClass('light')
-      $($boxes[index+a]).toggleClass('light')
+      $($boxes[index]).toggleClass('light');
+      $($boxes[index+1]).toggleClass('light');
+      $($boxes[index+a]).toggleClass('light');
     } else {
-      $($boxes[index]).toggleClass('light')
-      $($boxes[index+1]).toggleClass('light')
-      $($boxes[index-a]).toggleClass('light')
+      $($boxes[index]).toggleClass('light');
+      $($boxes[index+1]).toggleClass('light');
+      $($boxes[index-a]).toggleClass('light');
     }
   } else if (index % a === 4) {
     if (index > (a-1) && index < ($boxes.length)-1) {
-      $($boxes[index]).toggleClass('light')
-      $($boxes[index-a]).toggleClass('light')
-      $($boxes[index+a]).toggleClass('light')
-      $($boxes[index-1]).toggleClass('light')
+      $($boxes[index]).toggleClass('light');
+      $($boxes[index-a]).toggleClass('light');
+      $($boxes[index+a]).toggleClass('light');
+      $($boxes[index-1]).toggleClass('light');
     } else if (index === (a-1)) {
-      $($boxes[index]).toggleClass('light')
-      $($boxes[index-1]).toggleClass('light')
-      $($boxes[index+a]).toggleClass('light')
+      $($boxes[index]).toggleClass('light');
+      $($boxes[index-1]).toggleClass('light');
+      $($boxes[index+a]).toggleClass('light');
     } else {
-      $($boxes[index]).toggleClass('light')
-      $($boxes[index-a]).toggleClass('light')
-      $($boxes[index-1]).toggleClass('light')
+      $($boxes[index]).toggleClass('light');
+      $($boxes[index-a]).toggleClass('light');
+      $($boxes[index-1]).toggleClass('light');
     }
   } else if (index > 0 && index < (a-2)) {
-    $($boxes[index]).toggleClass('light')
-    $($boxes[index-1]).toggleClass('light')
-    $($boxes[index+1]).toggleClass('light')
-    $($boxes[index+a]).toggleClass('light')
+    $($boxes[index]).toggleClass('light');
+    $($boxes[index-1]).toggleClass('light');
+    $($boxes[index+1]).toggleClass('light');
+    $($boxes[index+a]).toggleClass('light');
   } else if (index >= ($boxes.length)-a && index < ($boxes.length)-1) {
-    $($boxes[index]).toggleClass('light')
-    $($boxes[index-1]).toggleClass('light')
-    $($boxes[index+1]).toggleClass('light')
-    $($boxes[index-a]).toggleClass('light')
+    $($boxes[index]).toggleClass('light');
+    $($boxes[index-1]).toggleClass('light');
+    $($boxes[index+1]).toggleClass('light');
+    $($boxes[index-a]).toggleClass('light');
   } else {
-    $($boxes[index]).toggleClass('light')
-    $($boxes[index-1]).toggleClass('light')
-    $($boxes[index+1]).toggleClass('light')
-    $($boxes[index-a]).toggleClass('light')
-    $($boxes[index+a]).toggleClass('light')
+    $($boxes[index]).toggleClass('light');
+    $($boxes[index-1]).toggleClass('light');
+    $($boxes[index+1]).toggleClass('light');
+    $($boxes[index-a]).toggleClass('light');
+    $($boxes[index+a]).toggleClass('light');
   }
+  litUp.checkWin();
 }
 
-litUp.alight = function(a, b, c, d, e) {
-  console.log(('li')[d])
-  $(('li')[a,b,c,d,e]).toggleClass("light");
+litUp.checkWin = function() {
+  if ($('li').hasClass('light') === false) {
+    console.log("you've won!");
+  }
 }
 
 $(function() { litUp.setup() });
